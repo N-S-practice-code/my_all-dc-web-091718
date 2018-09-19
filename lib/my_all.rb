9d4ad2dcf3e9b5a36a7f7ff46c9c3fe79b2f0dca
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-19 14:37:30 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,17 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
-  i = 0
-  block_return_values = []
+  # i = 0
+  # block_return_values = []
   # while i < collection.length
   #   block_return_values << yield(collection[i])
   #   i = i + 1
   # end
-  collection.each {|el|}
-  True
-  if block_return_values.include?(false)
-    false
-  else
-    true
-  end
+  collection.each {|el| return false if yield(el)}
+  true
+  # if block_return_values.include?(false)
+  #   false
+  # else
+  #   true
+  # end
 end
